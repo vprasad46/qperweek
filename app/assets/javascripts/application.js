843676e130c8c2ef2e72225c0737e24249508f95
vprasad46 <vprasad46@gmail.com>
2017-09-26 03:47:37 +0530

    Added design for the index page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,18 @@
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require_tree .
+
+$(document).ready(function(){
+	var myDate = new Date();
+	console.log(myDate);
+	myDate.setDate(myDate.getDate()+  );
+	myDate.setMins(myDate.getMins()+ ,myDate.getSeconds()+ );
+	myDate.setHours(myDate.getHours() + );
+	$("#countdown").countdown(myDate, function (event) {
+	  $(this).html(
+	      event.strftime(
+	          '<div class="timer-wrapper"><div class="time">%D</div><span class="text">DAYS </span></div><div class="timer-wrapper"><div class="time">%H</div><span class="text">HRS </span></div><div class="timer-wrapper"><div class="time">%M </div><span class="text">MINS </span></div><div class="timer-wrapper"><div class="time">%S</div><span class="text">SEC </span></div>'
+	      )
+	  );
+	});
+});
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,7 @@ Rails.application.configure do
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
-
+  config.assets.compress = true
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
@@ -29,7 +29,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-
+  config.assets.digest = true
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
