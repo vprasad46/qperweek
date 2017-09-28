7857c11b421e35ed8884754615f409686507bfe8
Vishwa Prasad R <vprasad46@gmail.com>
2017-09-28 12:22:46 +0000

    seeds.rb edited

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Deadline.create(endtime: 1.week.from_now)
